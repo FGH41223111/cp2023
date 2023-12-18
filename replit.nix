e403c1b8e9ea111922d14682c8043207c12f822e
FGH41223111 <144768585+FGH41223111@users.noreply.github.com>
2023-12-18 21:19:06 +0000

    add 單曲線圖

--- replit.nix
+++ replit.nix
@@ -1,7 +1,10 @@
-{pkgs}: {
-  deps = [
-    pkgs.libev
-  ];
+{ pkgs }: {
+    deps = [
+      pkgs.libev
+      pkgs.gnuplot
+      pkgs.ncurses.dev
+      pkgs.gd
+    ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.libev
